e2ab8c3fa346cc3d8e37be3d618afe3bba732a89
dmitritruf <dmitritruf@users.noreply.github.com>
2024-06-18 01:02:07 +0000

    Deploying to gh-pages from @ dmitritruf/fosscord-web@c0e28f4effe5a32c981bfcb6f74ec2e00bdbb452 🚀

---
+++ _nuxt/static/1718672524/instances/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1718672524",layout:"default",error:a,serverRendered:true,routePath:"\u002Finstances",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
