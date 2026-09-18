a9559755af7e6b1b0461d6e2f8db051408b36c9f
dmitritruf <dmitritruf@users.noreply.github.com>
2026-09-18 03:15:02 +0000

    Deploying to gh-pages from @ dmitritruf/fosscord-web@c0e28f4effe5a32c981bfcb6f74ec2e00bdbb452 🚀

---
+++ _nuxt/static/1789701300/contributors/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/contributors", {data:[{}],fetch:{"0":{members:[]},"1":{_error:{message:"invalid json response body at https:\u002F\u002Fraw.githubusercontent.com\u002Ffosscord\u002Ffosscord\u002Fmaster\u002Fassets\u002Fcontributors\u002Fcontributors.json reason: Unexpected token : in JSON at position 3",type:"invalid-json",statusCode:500}}},mutations:void 0});
